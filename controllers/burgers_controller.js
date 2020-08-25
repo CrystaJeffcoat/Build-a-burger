b5899471673cbc8f511fe36059ab2e82dd69cc13
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-25 17:24:49 -0400

    first page renders all burger from db

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const burger = require('../models/burger');
 /* GET home page. */
 
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function(req, res) {
+  burger.selectAll(function(data) {
+    let hbs = {
+      burgers: data
+    };
+    console.log(hbs);
+    res.render('index', hbs);
+  });
 });
 
 module.exports = router;
