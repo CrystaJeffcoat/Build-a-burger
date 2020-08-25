6c1b754d315d0332add9587b577315b6d6da227a
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-25 17:01:34 -0400

    added functions for orm to query data

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,29 @@
 const connection = require("./connection.js");
 
 const orm = {
-//   selectAll,
+  selectAll: function(table, cb) {
+    let queryString = "SELECT * FROM " + table + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
 
-//   insertOne,
+  insertOne: function(table, col, vals, cb) {
+    let queryString = `INSERT INTO ${table} (${col.toString()}) VALUES (${vals})`;
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
   
-//   updateOne
-}
+  updateOne: function(table, objCol, condition, cb) {
+    let queryString = `UPDATE ${table} SET ${objCol} WHERE ${condition}`;
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  }
+};
 
 module.exports = orm;
