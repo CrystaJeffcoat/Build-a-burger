7b21f6ce9da7889513abcdd47543aedcaa979690
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-26 19:49:20 -0400

    submit and eat buttons send ajax calls successfuly

--- public/javascripts/burger.js
+++ public/javascripts/burger.js
@@ -1,19 +1,42 @@
 $(function() {
   $("#submitBtn").on("submit", function(event) {
     event.preventDefault();
+    // check if value is empty
+   if(!$("#new-burger").val().length) {
+     return;
+   } else {
+      let burger = {
+        burger_name: "'" + $("#new-burger").val() + "'",
+        consumed: false
+      }
+      // api call to post new burger
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: burger
+      }).then(
+        function() {
+          console.log("created new burger!");
+          location.reload();
+        }
+      );
+    }
+  });
 
-    let burger = $("#new-burger").val().trim();
+  $(".eat-burger").click(function() {
+    let id = $(this).attr("data-id");
 
-    $.ajax("/api/burgers", {
-      type: "POST",
+    let burger = {
+      consumed: true
+    }
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
       data: burger
     }).then(
       function() {
-        console.log("created new burger!");
+        console.log("updated burger!");
         location.reload();
       }
     );
   });
-
-  //new button
 });
