f508cf130f2964febbf904dc5fd4c0059f6162ba
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-26 19:45:42 -0400

    api calls added to insert and update to db

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,9 +8,23 @@ router.get('/', function(req, res) {
     let hbs = {
       burgers: data
     };
-    console.log(hbs);
     res.render('index', hbs);
   });
 });
+
+router.post('/api/burgers', function(req, res) {
+  burger.insertOne("burger_name", req.body.burger_name, function(result) {
+    console.log(result);
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put('/api/burgers/:id', function(req, res) {
+  let condition = "id = " + req.params.id;
+  burger.updateOne("consumed = true", condition, function() {
+      res.status(200).end();
+    }
+  );
+})
 
 module.exports = router;
